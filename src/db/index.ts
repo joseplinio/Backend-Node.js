56496e0751bf69151d831b7600c6fc0601aca4fa
joseplinio <joseplinio8002@gmail.com>
2025-04-23 17:11:17 -0300

    Fazendo mudanças no db
    Criando tabelas users e com as dependencias de um arquivo drizzle

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,4 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
-import type { usersTable } from './schema';
 
-const db = drizzle(process.env.DATABASE_URL!)
-async function main() {
-  const user: typeof usersTable.$inferInsert = {
-    name: ''
-  }
-}
+export const db = drizzle(process.env.DATABASE_URL!)
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,8 @@
 import { integer, pgTable, varchar } from 'drizzle-orm/pg-core'
 
 export const usersTable = pgTable('users', {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: varchar().notNull().unique().primaryKey(),
   name: varchar().notNull(),
   age: integer().notNull(),
-  user_id: varchar({ length: 9 }).notNull().unique(),
+  hashPassWord: varchar().notNull()
 })
