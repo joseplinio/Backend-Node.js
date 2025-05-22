b636d30d63cea1667c5db47c3848a51a1abfd919
joseplinio <joseplinio8002@gmail.com>
2025-05-22 10:29:19 -0300

    Add os use-case do dele e add do user (achei que tinha adicionado) e mudadando o nome de respository (ies)

--- src/aplication/use_case/users/add.ts
+++ src/aplication/use_case/users/add.ts
@@ -1,15 +1,17 @@
-import { injectable, inject} from "tsyringe"
-import { UserRepository } from "../../../adapters/spi/repositorys/user-repository"
+import { inject, injectable } from "tsyringe"
+import { UserRepository } from "../../../adapters/spi/repositories/user-repository"
 import type { UserEntity } from "../../../domains/user-entity"
-import type { IUseCase } from "../case"
 import { IUserRepository } from "../../interface/repository/IUserRepository"
+import type { IUseCase } from "../case"
 
 @injectable() // Permite que o container injete coisas nessa classe
 export class UserAddCase implements IUseCase<UserEntity, UserEntity> {
 	// Pesso para montar a class com o UserRepository ja injetando sem fazer um new
-	constructor(@inject(UserRepository) private userRepository: IUserRepository) {}
+	constructor(
+		@inject(UserRepository) private userRepository: IUserRepository,
+	) {}
 
-  handler(body: UserEntity): Promise<UserEntity> {
-    return this.userRepository.add(body)
-  }
+	handler(body: UserEntity): Promise<UserEntity> {
+		return this.userRepository.add(body)
+	}
 }
--- src/aplication/use_case/users/delete.ts
+++ src/aplication/use_case/users/delete.ts
@@ -1,15 +1,15 @@
 import { inject, injectable } from "tsyringe"
+import { UserRepository } from "../../../adapters/spi/repositories/user-repository"
 import type { UserEntity } from "../../../domains/user-entity"
 import { IUserRepository } from "../../interface/repository/IUserRepository"
 import type { IUseCase } from "../case"
-import { UserRepository } from "../../../adapters/spi/repositorys/user-repository" 
 
 @injectable()
 export class DeleteUserCase implements IUseCase<string, UserEntity> {
 	constructor(
 		@inject(UserRepository) private userRepository: IUserRepository,
 	) {}
-  handler(body: string): Promise<UserEntity> {
-    return this.userRepository.delete(body)
-  }
+	handler(body: string): Promise<UserEntity> {
+		return this.userRepository.delete(body)
+	}
 }
