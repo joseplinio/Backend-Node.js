870e69abe29c687666308e96941ea9b3073cf9ce
joseplinio <joseplinio8002@gmail.com>
2025-06-13 19:17:49 -0300

    Add mundando a estrutura e fazendo o add do user no db com validaçao dto

--- src/adapters/api/http/users/controllers/add.ts
+++ src/adapters/api/http/users/controllers/add.ts
@@ -1,16 +1,55 @@
+import { randomUUID } from "crypto"
+import { StatusCodes } from "http-status-codes"
+import type { AdapterExpress } from "src/adapters/api/server/express/expressAdapter"
+import type { IUserValideDto } from "src/aplication/interface/dto/IUserValideDto"
+import type { IHashPassword } from "src/aplication/service/password/IHashPassword"
+import { CreateUserModel } from "src/aplication/use_case/TyCreaterUser"
+import type { CreaterUserDtoRequest } from "src/aplication/use_case/dto/dto"
 import { inject, injectable } from "tsyringe"
 import type { IController } from "../../../../../aplication/interface/controllers/IController"
 import type { IUseCase } from "../../../../../aplication/use_case/case"
-import { UserAddCase } from "../../../../../aplication/use_case/users/add"
 import type { UserEntity } from "../../../../../domains/user-entity"
-
 @injectable()
-export class UserAddController implements IController<UserEntity, UserEntity> {
+export class UserAddController implements IController<any, AdapterExpress> {
 	constructor(
-		@inject(UserAddCase) private userSercice: IUseCase<UserEntity, UserEntity>,
+		@inject("UserAddCase")
+		private userService: IUseCase<CreateUserModel, UserEntity>,
+		@inject("dtoValidator")
+		private dtoValidtor: IUserValideDto<CreaterUserDtoRequest, any>,
+		@inject("serviceHashPassword") private serviceHashPassword: IHashPassword,
 	) {}
 
-	handler(body: UserEntity): Promise<UserEntity> {
-		return this.userSercice.handler(body)
+	async handler(httpContext: AdapterExpress): Promise<void> {
+		try {
+			const request = await httpContext.getRequest()
+			const userInstance = await this.dtoValidtor.valideDto(request)
+
+			const hashedPasswd = await this.serviceHashPassword.hashPassword(
+				userInstance.hashpasswd,
+			)
+			const createUser: CreateUserModel = {
+				...userInstance,
+				id: randomUUID(),
+				hashpasswd: hashedPasswd,
+			}
+			this.userService.handler(createUser)
+			if (!createUser)
+				httpContext.send<any>(
+					StatusCodes.BAD_REQUEST,
+					"Erro ao fazer a permanencia do user",
+					null,
+				)
+
+			httpContext.send<any>(StatusCodes.CREATED, "User criado com sucesso", {
+				...userInstance,
+				hashpasswd: null,
+			})
+		} catch (err) {
+			httpContext.send<any>(
+				StatusCodes.BAD_REQUEST,
+				"Erro ao criar o user",
+				err,
+			)
+		}
 	}
 }
