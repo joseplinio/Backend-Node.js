6f0506c4cd3cbb63c427471228cf41584682535a
joseplinio <joseplinio8002@gmail.com>
2025-06-28 13:56:31 -0300

    (Mod) Mudando como eu faço para deixar o db tipado e suas var liga com o .env

--- src/adapters/spi/drizzle/db/db.ts
+++ src/adapters/spi/drizzle/db/db.ts
@@ -1,4 +1,7 @@
 import "dotenv/config"
 import { drizzle } from "drizzle-orm/node-postgres"
+import { Pool } from "pg"
+import * as schema from "./schema"
 
-export const db = drizzle(process.env.DATABASE_URL!)
+const poll = new Pool({ connectionString: process.env.DATABASE_URL! })
+export const db = drizzle(poll, { schema })
