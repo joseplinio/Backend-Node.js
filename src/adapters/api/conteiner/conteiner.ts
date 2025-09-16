6a244dde77e3126516c3051c8bdb3c647db95c43
José Plinio Ripardo Rocha <joseplinio8002@gmail.com>
2025-09-16 14:13:21 -0300

    (Mud) Mudando a org e modelo de instancias criadas

--- src/adapters/api/conteiner/conteiner.ts
+++ src/adapters/api/conteiner/conteiner.ts
@@ -1,21 +1,37 @@
-import { UserRepository } from "src/adapters/spi/repositories/user-repository"
-import serviceHashPassword from "src/aplication/service/password/passwordService"
+import { TokenRepository } from "src/adapters/spi/repositories/tokenRepository"
+import { UserRepository } from "src/adapters/spi/repositories/userRepository"
+import { JwtService } from "src/aplication/service/auth/jwt/jwtService"
+import serviceHashItem from "src/aplication/service/auth/password/hashItemService"
 import { UserAddCase } from "src/aplication/use_case/users/add"
 import { UserDeleteCase } from "src/aplication/use_case/users/delete"
 import { DtoDeleteUser } from "src/aplication/use_case/users/dto/dtoDeleteUser"
 import { DtoValidator } from "src/aplication/use_case/users/dto/valideDto"
 import { UserFindCase } from "src/aplication/use_case/users/find"
 import { UserListCase } from "src/aplication/use_case/users/list"
-import { container } from "tsyringe"
-import { UserAddController } from "../http/users/controllers/add"
-import { UserDeleteController } from "../http/users/controllers/delete"
-import { UserFindController } from "../http/users/controllers/find"
-import { UserListController } from "../http/users/controllers/list"
-import { AdapterExpress } from "../server/express/expressAdapter"
 import { UserLoginCase } from "src/aplication/use_case/users/login"
-import { UserLoginController } from "../http/users/controllers/login"
+import { container } from "tsyringe"
+import { UserAddController } from "../http/users/controllers/routers/add"
+import { UserDeleteController } from "../http/users/controllers/routers/delete"
+import { UserFindController } from "../http/users/controllers/routers/find"
+import { UserListController } from "../http/users/controllers/routers/list"
+import { UserLoginController } from "../http/users/controllers/routers/login"
 
-// Controllers
+// RegisterSingleton
+container.registerSingleton<UserRepository>("UserRepository", UserRepository)
+container.registerSingleton<TokenRepository>("TokenRepository", TokenRepository)
+container.registerSingleton<JwtService>("JwtService", JwtService)
+
+// Register
+container.register<UserFindController>("UserFindController", UserFindController)
+container.register<UserListController>("UserListController", UserListController)
+container.register<DtoValidator>("DtoValidator", DtoValidator)
+container.register<DtoDeleteUser>("DtoDeleteUser", DtoDeleteUser)
+container.register<serviceHashItem>("serviceHashItem", serviceHashItem)
+container.register<UserLoginCase>("UserLoginCase", UserLoginCase)
+container.register<UserDeleteCase>("UserDeleteCase", UserDeleteCase)
+container.register<UserAddCase>("UserAddCase", UserAddCase)
+container.register<UserListCase>("UserListCase", UserListCase)
+container.register<UserFindCase>("UserFindCase", UserFindCase)
 container.register<UserAddController>("UserAddController", UserAddController)
 container.register<UserDeleteController>(
 	"UserDeleteController",
@@ -25,24 +41,3 @@ container.register<UserLoginController>(
 	"UserLoginController",
 	UserLoginController,
 )
-container.register<UserFindController>("UserFindController", UserFindController)
-container.register<UserListController>("UserListController", UserListController)
-// Dto e Adatepter Servicer
-container.register<DtoValidator>("DtoValidator", DtoValidator)
-container.register<DtoDeleteUser>("DtoDeleteUser", DtoDeleteUser)
-
-container.register<serviceHashPassword>(
-	"serviceHashPassword",
-	serviceHashPassword,
-)
-container.register<AdapterExpress>("AdapterExpress", AdapterExpress)
-
-// Repositors
-container.register<UserRepository>("UserRepository", UserRepository)
-
-// Cases
-container.register<UserLoginCase>("UserLoginCase", UserLoginCase)
-container.register<UserDeleteCase>("UserDeleteCase", UserDeleteCase)
-container.register<UserAddCase>("UserAddCase", UserAddCase)
-container.register<UserListCase>("UserListCase", UserListCase)
-container.register<UserFindCase>("UserFindCase", UserFindCase)
