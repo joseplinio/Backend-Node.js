02dffecc0f97c16cac8be0e372db16a1bdd8162e
José Plinio Ripardo Rocha <joseplinio8002@gmail.com>
2025-09-30 14:40:38 -0300

    (Add) the refresh endpoint / router

--- src/adapters/api/server/express/routers/auth/authRoutery.ts
+++ src/adapters/api/server/express/routers/auth/authRoutery.ts
@@ -1,5 +1,6 @@
 import { type Request, type Response, Router } from "express"
-import { UserLoginController } from "src/adapters/api/http/users/controllers/routers/login"
+import { UserLoginController } from "src/adapters/api/http/auth/controllers/login"
+import { RefreshTokenController } from "src/adapters/api/http/auth/controllers/refresh"
 import { container } from "tsyringe"
 import { AdapterExpress } from "../../expressAdapter"
 
@@ -8,4 +9,9 @@ export const authRouter = Router()
 authRouter.post("/login", async (req: Request, res: Response) => {
 	const adapterEx = new AdapterExpress(req, res)
 	container.resolve(UserLoginController).handler(adapterEx)
+})
+
+authRouter.post("/refresh", async (req: Request, res: Response) => {
+	const adapterEx = new AdapterExpress(req, res)
+	container.resolve(RefreshTokenController).handler(adapterEx)
 })
