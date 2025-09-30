7ea814bf9a3944871b409757c562ff8fd40599ee
José Plinio Ripardo Rocha <joseplinio8002@gmail.com>
2025-09-30 14:38:41 -0300

    (Mod) some rouhter, but not the Add

--- src/adapters/api/server/express/routers/user/userRoutery.ts
+++ src/adapters/api/server/express/routers/user/userRoutery.ts
@@ -1,11 +1,11 @@
 import { type Request, type Response, Router } from "express"
-import { UserAddController } from "src/adapters/api/http/users/controllers/routers/add"
+import { UserAddController } from "src/adapters/api/http/users/controllers/add"
 import { container } from "tsyringe"
 import { AdapterExpress } from "../../expressAdapter"
 
 export const userRouter = Router()
 
-userRouter.post("/", async (req: Request, res: Response) => {
+userRouter.post("/add", async (req: Request, res: Response) => {
 	const adapterEx = new AdapterExpress(req, res)
 	container.resolve(UserAddController).handler(adapterEx)
 })
